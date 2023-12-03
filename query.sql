d8b07ebdabf3ea777668c392c79f400c633cc9f8
Jacob Trock <jtbosshku@gmail.com>
2023-12-03 17:01:43 -0500

    schema changes

--- query.sql
+++ query.sql
@@ -1,18 +1,44 @@
--- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = ? LIMIT 1;
+-- name: AddCropBuyer :exec
+INSERT INTO Crop_Buyer (Name, Quantities_Required, Crop_Type, Target_Price)
+VALUES ($1, $2, $3, $4);
 
--- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
+-- name: AddFarmer :exec
+INSERT INTO Farmer (Name, Budget, Net_Worth, Farm_ID, Purchase_ID)
+VALUES ($1, $2, $3, $4, $5);
 
--- name: CreateAuthor :execresult
-INSERT INTO authors (
-  name, bio
-) VALUES (
-  ?, ?
-);
+-- name: AddFarm :exec
+INSERT INTO Farm (Name, Farm_Value, Address_Street, Address_City, Address_State, Address_Zip)
+VALUES ($1, $2, $3, $4, $5, $6);
 
--- name: DeleteAuthor :exec
-DELETE FROM authors
-WHERE id = ?;
+-- name: AddHarvest :exec
+INSERT INTO Harvest (
+  Quantity,
+  Time_Year,
+  Time_Season,
+  Ph_Base,
+  Ph_Fertilized,
+  Water_Rain,
+  Water_Sprinkler,
+  Sun,
+  Price,
+  Crop_Type,
+  Farm_ID,
+  Extinct
+)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
+
+-- name: AddMonitorsBuyer :exec
+INSERT INTO Monitors_Buyer (Name, Crop_Type)
+VALUES ($1, $2);
+
+-- name: AddInvestsIn :exec
+INSERT INTO Invests_In (Name, Farm_ID)
+VALUES ($1, $2);
+
+-- name: AddEnforces :exec
+INSERT INTO Enforces (USDAID, Code_ID)
+VALUES ($1, $2);
+
+-- name: AddInvestMonitor :exec
+INSERT INTO Monitors_Investments (Name, Crop_Type)
+VALUES ($1, $2);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,137 @@
-CREATE TABLE authors (
-  id   BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name text    NOT NULL,
-  bio  text
+CREATE TABLE IF NOT EXISTS Farm
+(
+  Name           VARCHAR(255) NOT NULL,
+  Farm_Value      INT,
+  Farm_ID         INT,
+  Address_Street VARCHAR(255),
+  Address_City   VARCHAR(255),
+  Address_State  VARCHAR(255),
+  Address_Zip    VARCHAR(10),
+  PRIMARY KEY (Farm_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Crop
+(
+  Crop_Type            VARCHAR(255),
+  Ph_Range_Weight      DECIMAL(5, 2),
+  Ph_Range_Desired     DECIMAL(5, 2),
+  Water_Needed_Weight  DECIMAL(5, 2),
+  Water_Needed_Desired DECIMAL(5, 2),
+  Sun_Range_Weight     DECIMAL(5, 2),
+  SunRange_Desired    DECIMAL(5, 2),
+  Banned	      BOOLEAN,
+  PRIMARY KEY (Crop_Type)
+);
+
+CREATE TABLE IF NOT EXISTS Purchase
+(
+  Purchase_ID       INT AUTO_INCREMENT,
+  Crop_Type         VARCHAR(255),
+  Purchase_Complete BOOLEAN,
+  Total_Price       DECIMAL(5, 2),
+  Total_Quantity    INT,
+  Purchase_Date     DATE,
+  PRIMARY KEY (Purchase_ID),
+  FOREIGN KEY (Crop_Type) REFERENCES Crop (Crop_Type)
+);
+
+CREATE TABLE IF NOT EXISTS Crop_Investor
+(
+  Name            VARCHAR(255) NOT NULL,
+  Buy_Price        INT,
+  Investible_Money INT,
+  Sell_Price       INT,
+  PRIMARY KEY (Name)
+);
+
+CREATE TABLE IF NOT EXISTS Crop_Investigator
+(
+  Name   VARCHAR(255) NOT NULL,
+  USDAID INT,
+  PRIMARY KEY (USDAID)
+);
+
+CREATE TABLE IF NOT EXISTS District_Code
+(
+  Max_Water INT,
+  Max_Fert  INT,
+  Crop_Type VARCHAR(255),
+  Code_ID   INT,
+  PRIMARY KEY (Code_ID),
+  FOREIGN KEY (Crop_Type) REFERENCES Crop (Crop_Type)
+);
+
+CREATE TABLE IF NOT EXISTS Crop_Buyer
+(
+  Name                VARCHAR(255),
+  Quantities_Required INT,
+  Crop_Type            VARCHAR(255),
+  Target_Price         INT,
+  PRIMARY KEY (Name),
+  FOREIGN KEY (Crop_Type) REFERENCES Crop (Crop_Type)
+);
+
+CREATE TABLE IF NOT EXISTS Farmer
+(
+  Name          VARCHAR(255) NOT NULL,
+  Budget        INT,
+  Net_Worth      INT,
+  Farm_ID        INT,
+  Purchase_ID    INT,
+  -- PRIMARY KEY (Name),
+  FOREIGN KEY (Farm_ID) REFERENCES Farm (Farm_ID),
+  FOREIGN KEY (Purchase_ID) REFERENCES Purchase (Purchase_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Harvest
+(
+  Quantity        INT,
+  Time_Year       YEAR,
+  Time_Season     VARCHAR(255),
+  Ph_Base         DECIMAL(5, 2),
+  Ph_Fertilized   DECIMAL(5, 2),
+  Water_Rain      DECIMAL(5, 2),
+  Water_Sprinkler DECIMAL(5, 2),
+  Sun             INT,
+  Price           DECIMAL(5, 2),
+  Crop_Type        VARCHAR(255),
+  Farm_ID          INT,
+  Extinct         BOOLEAN,
+  FOREIGN KEY (Crop_Type) REFERENCES Crop (Crop_Type),
+  FOREIGN KEY (Farm_ID) REFERENCES Farm (Farm_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Monitors_Buyer
+(
+  Name     VARCHAR(255) NOT NULL,
+  Crop_Type VARCHAR(255) NOT NULL,
+  FOREIGN KEY (Name) REFERENCES Crop_Buyer (Name),
+  FOREIGN KEY (Crop_Type) REFERENCES Crop (Crop_Type)
+);
+
+CREATE TABLE IF NOT EXISTS Invests_In
+(
+  Name   VARCHAR(255),
+  Farm_ID INT,
+  -- PRIMARY KEY (Name),
+  FOREIGN KEY (Name) REFERENCES Crop_Investor (Name),
+  FOREIGN KEY (Farm_ID) REFERENCES Farm(Farm_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Enforces
+(
+  USDAID INT NOT NULL,
+  Code_ID INT NOT NULL,
+  -- PRIMARY KEY (USDAID, CodeID),
+  FOREIGN KEY (USDAID) REFERENCES Crop_Investigator (USDAID),
+  FOREIGN KEY (Code_ID) REFERENCES District_Code (Code_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Monitors_Investments
+(
+  Name     VARCHAR(255),
+  Crop_Type VARCHAR(255),
+  -- PRIMARY KEY (Name),
+  FOREIGN KEY (Name) REFERENCES Crop_Investor (Name),
+  FOREIGN KEY (Crop_Type) REFERENCES Harvest (Crop_Type)
 );
