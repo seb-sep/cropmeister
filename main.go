1c79b1f1efddb87da6d6d88059b7199a70035b14
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-12-03 16:01:42 -0500

    route stubs

--- main.go
+++ main.go
@@ -36,5 +36,134 @@ func main() {
 		return c.JSON(names)
 	})
 
+	app.Get("/harvest", func(c *fiber.Ctx) error {
+		return c.JSON("harvest")
+	})
+
+	app.Post("/harvest", func(c *fiber.Ctx) error {
+		return c.JSON("harvest")
+	})
+
+	app.Get("/crop/:type", func(c *fiber.Ctx) error {
+		return c.JSON("crop")
+	})
+	app.Put("/crop/:type", func(c *fiber.Ctx) error {
+		return c.JSON("crop")
+	})
+	app.Delete("/crop/:type", func(c *fiber.Ctx) error {
+		return c.JSON("crop")
+	})
+
+	app.Get("/farm", func(c *fiber.Ctx) error {
+		return c.JSON("farm")
+	})
+	app.Post("/farm", func(c *fiber.Ctx) error {
+		return c.JSON("farm")
+	})
+
+	app.Get("/farm/:id", func(c *fiber.Ctx) error {
+		return c.JSON("farm")
+	})
+
+	app.Put("/farm/:id", func(c *fiber.Ctx) error {
+		return c.JSON("farm")
+	})
+
+	app.Delete("/farm/:id", func(c *fiber.Ctx) error {
+		return c.JSON("farm")
+	})
+
+	app.Get("/harvest/:type", func(c *fiber.Ctx) error {
+		return c.JSON("harvest")
+	})
+	app.Put("/harvest/:type", func(c *fiber.Ctx) error {
+		return c.JSON("harvest")
+	})
+	app.Delete("/harvest/:type", func(c *fiber.Ctx) error {
+		return c.JSON("harvest")
+	})
+
+	app.Get("/purchase", func(c *fiber.Ctx) error {
+		return c.JSON("purchase")
+	})
+
+	app.Post("/purchase", func(c *fiber.Ctx) error {
+		return c.JSON("purchase")
+	})
+
+	app.Get("/purchase/:id", func(c *fiber.Ctx) error {
+		return c.JSON("purchase")
+	})
+	app.Put("/purchase/:id", func(c *fiber.Ctx) error {
+		return c.JSON("purchase")
+	})
+	app.Delete("/purchase/:id", func(c *fiber.Ctx) error {
+		return c.JSON("purchase")
+	})
+
+	app.Get("/farmer/:id", func(c *fiber.Ctx) error {
+		return c.JSON("farmer")
+	})
+
+	app.Post("/farmer/:id", func(c *fiber.Ctx) error {
+		return c.JSON("farmer")
+	})
+
+	app.Get("/cropinspector", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+	app.Post("/cropinspector", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+
+	app.Get("/districtcode/crop/:type", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+	app.Post("/districtcode/crop/:type", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+
+	app.Get("/districtcode/inspector/:id", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+	app.Post("/districtcode/inspector/:id", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+
+	app.Get("/districtcode/code/:id", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+	app.Put("/districtcode/code/:id", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+	app.Delete("/districtcode/code/:id", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+
+	app.Get("/districtcode", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+
+	app.Post("/districtcode", func(c *fiber.Ctx) error {
+		return c.JSON("districtcode")
+	})
+
+	app.Get("/cropinspector/:id", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+	app.Put("/cropinspector/:id", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+	app.Get("/cropinspector/:id", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+
+	app.Get("/cropinspector/code/:id", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+	app.Post("/cropinspector/code/:id", func(c *fiber.Ctx) error {
+		return c.JSON("cropinspector")
+	})
+
 	app.Listen(":3000")
 }
