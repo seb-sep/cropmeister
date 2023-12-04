b3ee3a9f62f8c4ed5e0f7e157912d14e8874428f
cec777222 <douglas.ca@northeastern.edu>
2023-12-03 19:56:00 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -42,3 +42,90 @@ VALUES (?,?);
 -- name: AddInvestMonitor :exec
 INSERT INTO Monitors_Investments (Name, Crop_Type)
 VALUES (?,?);
+
+-- /Farmer/{FarmID}
+-- Get
+SELECT *
+FROM Farmer
+WHERE FarmID = ?;
+-- Post
+INSERT INTO Farmer (Name, Budget, NetWorth, FarmID, PurchaseID)
+SELECT *
+FROM Farmer
+WHERE FarmID = ?;
+
+-- /CropInspector
+-- Get
+SELECT *
+FROM Crop_Investigator;
+-- Post
+INSERT INTO Crop_Investigator (Name, USDAID)
+VALUES (?, ?);
+
+-- /DistinctCode/Crop/{CropType}
+-- Get
+SELECT *
+FROM District_Code
+WHERE CropType = ?;
+-- Post
+INSERT INTO District_Code (MaxWater, MaxFert, CropType, CodeID)
+SELECT *
+FROM District_Code
+WHERE CropType = ?;
+
+-- /DistrictCode/Inspector/{USDAID}
+-- Get
+SELECT *
+FROM District_Code
+WHERE farm.Crop_Investigator.USDAID = ?;
+-- Post
+INSERT INTO District_Code (MaxWater, MaxFert, CropType, CodeID)
+SELECT *
+FROM District_Code
+WHERE farm.Crop_Investigator.USDAID = ?;
+
+-- /CropInspector/Code/{CodeID}
+-- Get
+SELECT *
+FROM Crop_Investigator
+WHERE farm.District_Code.CodeID = ?;
+-- Post
+INSERT INTO Crop_Investigator (Name, USDAID)
+SELECT *
+FROM Crop_Investigator
+WHERE farm.District_Code.CodeID = ?;
+
+-- /CodeInspector/{USDAID}
+-- Get
+SELECT *
+FROM Crop_Investigator
+WHERE USDAID = ?;
+-- Put
+UPDATE Crop_Investigator
+SET Name = ?
+WHERE USDAID = ?;
+-- Delete
+DELETE FROM Crop_Investigator
+WHERE USDAID = ?;
+
+-- /DistrictCode
+-- Get
+SELECT *
+FROM District_Code;
+-- Post
+INSERT INTO District_Code (MaxWater, MaxFert, CropType, CodeID)
+VALUES (?, ?, ?, ?);
+
+-- DistrictCode/Code/{CodeID}
+-- Get
+SELECT *
+FROM District_Code
+WHERE CodeID = ?;
+-- Put
+UPDATE District_Code
+SET MaxWater = ?, MaxFert = ?,
+    CropType = ?, CodeID = ?
+WHERE CodeID = ?;
+-- Delete
+DELETE FROM District_Code
+WHERE CodeID = ?;
