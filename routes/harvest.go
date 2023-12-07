4b32b0c9d52007596aa3c1e8d5cb3d0785fe5185
cec777222 <douglas.ca@northeastern.edu>
2023-12-07 09:59:41 -0500

    Update harvest.go

--- routes/harvest.go
+++ routes/harvest.go
@@ -1,27 +1,50 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func HarvestRoutes(harvest fiber.Router) {
 	harvest.Get("", func(c *fiber.Ctx) error {
-		return c.JSON("harvest")
+		queries := c.Locals("db").(*db.Queries)
+		harvests, err := queries.harvests(ctx)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(harvests)
 	})
 	harvest.Post("", func(c *fiber.Ctx) error {
-		return c.JSON("harvest")
+		harvest, err := queries.harvest(ctx)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(harvest)
 	})
 	harvest.Get("/:farm/:type", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("type")
+		queries := c.Locals("db").(*db.Queries)
+		
+		type, err := c.ParamsStr("type")
+		harvest, err := queries.
 		if err != nil {
 			return c.Status(400).SendString(err.Error())
 		}
-		return c.JSON("harvest")
+		return c.JSON(harvest)
 	})
 	harvest.Put("/:farm/:type", func(c *fiber.Ctx) error {
+		queries := c.Locals("db").(*db.Queries)
+		
 		return c.JSON("harvest")
 	})
 	harvest.Delete("/:type", func(c *fiber.Ctx) error {
-		return c.JSON("harvest")
+		queries := c.Locals("db").(*db.Queries)
+		type, err := c.ParamsStr("type")
+		
+		harvest, err := queries.
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(harvest)
 	})
 }
